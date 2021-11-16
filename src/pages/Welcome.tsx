8ee7f311c136089b32b60ddf4f127b4b5b1ee30f
João Bebber <joao.gbebber@gmail.com>
2021-11-16 12:48:21 -0300

    fix: initial page style

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,74 +1,103 @@
-import React, { useState } from "react";
+import React from 'react';
 import {
         SafeAreaView,
         Text,
         Image,
-        StyleSheet
+        StyleSheet,
+        TouchableOpacity,
+        Dimensions,
+        View
 } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.svg';
-import { Button } from "../components/Button";
-import colors from "../styles/colors";
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome () {
-        const [visible, setVisible] = useState(true);
-
-        function handleVisibility () {
-                setVisible(true);
-        }
-
         return (
                 <SafeAreaView style={styles.container}>
+                        <View style={styles.wrapper}>
 
-                        <Text style={styles.title}>
-                                Gerencie {'\n'}
-                                suas plantas de {'\n'}
-                                forma fácil
-                        </Text>
+                                <Text style={styles.title}>
+                                        Gerencie {'\n'}
+                                        suas plantas de {'\n'}
+                                        forma fácil
+                                </Text>
 
-                        {
-                                visible && 
                                 <Image
                                         source={ wateringImg }
                                         style={styles.image}
+                                        resizeMode="contain"
                                 />
-                        }
 
-                        <Text style={styles.subtitle}>
-                                Não esqueça mais de regar suas plantas.
-                                Nós cuidamos de lembrar você sempre que precisar.
-                        </Text>
+                                <Text style={styles.subtitle}>
+                                        Não esqueça mais de regar suas plantas.
+                                        Nós cuidamos de lembrar você sempre que precisar.
+                                </Text>
 
-                        <Button title=">" />
+                                <TouchableOpacity
+                                        style={styles.button}
+                                        activeOpacity={0.7}
+                                >
+                                        <Entypo
+                                                name="chevron-right"
+                                                style={styles.buttonIcon}
+                                        />
+                                </TouchableOpacity>
 
+                        </View>
                 </SafeAreaView>
         );
 }
 
 const styles = StyleSheet.create({
         container: {
+                flex: 1
+        },
+
+        wrapper: {
                 flex: 1,
                 alignItems: 'center',
-                justifyContent: 'space-between'
+                justifyContent: 'space-around',
+                paddingHorizontal: 20
         },
 
         title: {
-                fontSize: 32,
+                fontSize: 28,
                 fontWeight: 'bold',
                 textAlign: 'center',
                 color: colors.heading,
-                marginTop: 38
+                marginTop: 38,
+                fontFamily: fonts.heading,
+                lineHeight: 34
         },
 
         subtitle: {
                 textAlign: 'center',
                 fontSize: 18,
                 paddingHorizontal: 20,
-                color: colors.heading
+                color: colors.heading,
+                fontFamily: fonts.text
         },
 
         image: {
-                width: 292,
-                height: 284
+                height: Dimensions.get('window').width * 0.7
+        },
+
+        button: {
+                backgroundColor: colors.green,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 16,
+                marginBottom: 10,
+                height: 56,
+                width: 56,
+                paddingHorizontal: 10
+        },
+
+        buttonIcon: {
+                fontSize: 32,
+                color: colors.white
         }
 });
